4caddfa24fa4e6c630e952cc2bb13a7c48813882
johnathanbere <johnbere@live.co.uk>
2015-11-05 18:58:36 +0000

    Reimplemented Contacts DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101193149) do
+ActiveRecord::Schema.define(version: 20151105185556) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
