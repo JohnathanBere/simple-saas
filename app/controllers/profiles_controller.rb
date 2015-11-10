d11ac73e57801d64068691fb3c58138068a2ec03
johnathanbere <johnbere@live.co.uk>
2015-11-10 23:39:19 +0000

    Added an update action in the profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,6 +21,17 @@ class ProfilesController < ApplicationController
         @profile = @user.profile
     end
     
+    def update
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path( params[:user_id] )
+        else
+            render action: :edit
+        end
+    end
+    
     private
         def profile_params #Whitelists whatever is on the form! Adding more table attributes means adding them on here as well!
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
