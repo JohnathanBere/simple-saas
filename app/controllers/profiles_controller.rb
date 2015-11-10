862b1299a0f3934b955652af0c24eee328ec05f9
johnathanbere <johnbere@live.co.uk>
2015-11-10 19:27:26 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,20 @@ class ProfilesController < ApplicationController
         @user = User.find( params[:user_id] )
         @profile = @user.build_profile
     end
+    
+    def create_table
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path( params[:user_id] )
+        else
+            render action :new
+        end
+    end
+    
+    private
+        def profile_params #Whitelists whatever is on the form! Adding more table attributes means adding them on here as well!
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
 end
